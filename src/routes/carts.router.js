3e20b70e773fa706f5d5c1af5b9db9102dae7183
William Humberto Herrera Rey <herrerawilliamh@gmail.com>
2023-09-14 17:13:16 -0500

    Subida de ruta carts

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -5,22 +5,6 @@ const router = express.Router();
 const productManager = new ProductManager();
 const carts = productManager.getProducts();
 
-router.get("/", (req, res) => {
-    const limit = req.query.limit || products.length;
-    res.json(products.slice(0, limit));
-})
 
-router.get("/:pid", (req, res) => {
-    const idProduct = parseInt(req.params.pid);
-    const product = products.find(p => p.id === idProduct);
-    if(!product) return res.send({error: "Producto no encontrado"});
-    res.send({ product });
-})
-
-router.post("/", (req, res)=>{
-    const {title, description, price, thumbnail, code, stock} = req.body;
-    productManager.addProduct(title, description, price, thumbnail, code, stock);
-    res.send("Producto agregado exitosamente");
-})
 
 export default router;
